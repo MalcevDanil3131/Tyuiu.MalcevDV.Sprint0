8e0a524d81cf709bfbf34a13c26df1e28307b2e7
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-09-14 15:03:16 +0500

    Task5

---
+++ Tyuiu.MalcevDV.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.MalcevDV.Sprit0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0}, на ноль делиь нельзя ", b);
+                return -1;
+            }
+            else
+            {
+                return (a / b);
+            }
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.MalcevDV.Sprit0.Task4.V0.Lib;
+Console.WriteLine("a + b = " + DataService.Addition(100, 200));
+Console.WriteLine("a - b = " + DataService.Substraction(356, 200));
+Console.WriteLine("a * b = " + DataService.Multiplication(10, 10));
+Console.WriteLine("a / b = " + DataService.Divide(100, 10));
